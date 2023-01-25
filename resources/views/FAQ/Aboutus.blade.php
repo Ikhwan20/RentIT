eda7b23ddaee262a624ce29bb227f37171860116
HAARPERFT <rajashawal246@gmail.com>
2023-01-25 21:33:43 +0800

    about us

--- resources/views/FAQ/Aboutus.blade.php
+++ resources/views/FAQ/Aboutus.blade.php
@@ -1,22 +1,27 @@
 <x-app-layout>
-    <div style="background-color:#AED6F1">
-    <style>
-      h3,p,h2{
-        padding-left:5%;
-      }
-      form
-      {
-        text-align: center;
-      }
-      </style>
-      <h1 style="text-align:center";><b>About Us</b></h1>
-      <br>
-      <h2>Who we are? </h2>
-      <p>Our public platform <b>serves more than 10000 people every year</b>, making us as one of popular startup company in Malaysia </p>
-      <p>Our websites offer thousand of people to earn their side-income easily as easy as pie!</p>
-      
-      <img src="https://i0.wp.com/websenor.com/wp-content/uploads/2021/10/work-img-1.png?w=483&ssl=1"
-  <br><br>
-     <br><p>Our goal is to help people that cannot afford to buy a basic neccesities such as kettle and hair dryer by giving them an alternative to rent it the items</p>
-  
-  </x-app-layout>
+  <div style="background-color:#AED6F1; padding: 20px">
+  <style>
+    h2, h3, p {
+      padding-left:5%;
+    }
+    form {
+      text-align: center;
+    }
+    img {
+      display: block;
+      margin: 0 auto;
+      width: 50%;
+    }
+    </style>
+    <h1 style="text-align:center; padding-bottom: 20px;"><b>About Us</b></h1>
+    <h2 style="padding-bottom: 10px;">Who we are? </h2>
+    <p>Our public platform <b>serves more than 10000 people every year</b>, making us one of the popular startup companies in Malaysia. We are a utilities rental website that offers a wide range of items for rent, including home appliances, electronics, and outdoor equipment.</p>
+    <p style="padding-bottom: 10px;">Our website is easy to use and offers flexible rental options to fit your needs. Whether you need to rent a camera for a photoshoot, a generator for an event, or a lawnmower for the weekend, we have you covered.</p>
+    <img src="https://i0.wp.com/websenor.com/wp-content/uploads/2021/10/work-img-1.png?w=483&ssl=1">
+    <br>
+    <h3 style="padding-bottom: 10px;">Our Mission</h3>
+    <p>Our goal is to help people that cannot afford to buy basic necessities such as kettle and hair dryer by giving them an alternative to rent it the items, or for people who only use it for a short period of time. We also offer a cost-effective solution for those who need equipment for a one-time event or project.</p>
+    <h3 style="padding-bottom: 10px;">Customer Service</h3>
+    <p>We pride ourselves on our wide selection of items and our excellent customer service. Our team is dedicated to helping you find the right item for your needs and ensuring a smooth rental process. Try us out today and see how we can help you save money and make your life easier.</p>
+   </div>
+</x-app-layout>
